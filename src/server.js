fff5560f8d3b9d5099953a0ad5a428a9cc90d928
aarias <aarias@consensussa.com>
2022-11-28 16:48:01 -0500

    despliegue heroku

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/ng-blog'));
+app.use(express.static(__dirname+'/dist/pagos-dibranet'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/pagos-dibranet/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
